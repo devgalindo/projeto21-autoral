bcb3bb0c7da0671253a535caff8dab766c8e3874
Galliss13 <matheusgalindo1313@gmail.com>
2023-03-29 15:13:01 -0300

    feat: signin and signup customization by sx

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Button, Container, Link, TextField, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { sxBox, sxButton, sxContainer, sxTextField } from './styles';
 
 function SigninPage() {
+  const navigate = useNavigate()
   return (
-    <div>SigninPage</div>
-  )
+    <Container maxWidth={'sm'} sx={sxContainer}>
+      <Box component="form" sx={sxBox}>
+        <Typography>Faça Login</Typography>
+        <TextField margin='dense' type={'password'} label='Email' sx={sxTextField}></TextField>
+        <TextField margin='dense' type={'password'} label='Password' sx={sxTextField}></TextField>
+        <Button size='large' variant='contained' sx={sxButton}>Faça Login</Button>
+        <Link onClick={() => navigate('/auth/signup')} mt={2} sx={{cursor:'pointer'}}>Ainda não tem uma conta? Cadastre-se</Link>
+      </Box>
+    </Container>
+  );
 }
 
-export default SigninPage
+export default SigninPage;
